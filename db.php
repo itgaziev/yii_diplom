27795bc0a9bb2173551135a09de3ba9c6f1f2450
Im Foxy Dev <gaziev.ilyos@yandex.ru>
2024-05-30 18:10:08 +0300

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=u429579.mysql.masterhost.ru;dbname=u429579_1735',
-    'username' => 'u429579_aleksey',
+    'username' => 'u429579_db',
     'password' => '_a-2strUpYcH9L',
     'charset' => 'utf8',
 ];
